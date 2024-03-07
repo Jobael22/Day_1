3f1fadcd603b267117fa12824431c7b688cc8a5e
Jobael Ahmed Nishat <159699848+Jobael22@users.noreply.github.com>
2024-03-07 17:11:13 +0600

    Update script0.js

--- script0.js
+++ script0.js
@@ -1,8 +1,26 @@
-console.log(string_to_array("Robin Singh"));
+let inputString = "Robin Singh";
 
-function string_to_array(input)
-{
-let array = input.split(" ");
+let words = [];
 
-return array;
+let currentWord = "";
+
+for (let i = 0; i < inputString.length; i++) {
+    let char = inputString[i];
+
+    // Check if the character is an alphanumeric character or an apostrophe
+    if (/[a-zA-Z0-9']/.test(char)) {
+        currentWord += char;
+        
+    } else if (currentWord !== "") {
+        // Add the current word to the array when a non-alphanumeric character is encountered
+        words.push(currentWord);
+        currentWord = "";
+    }
 }
+
+// Add the last word if the string doesn't end with a non-alphanumeric character
+if (currentWord !== "") {
+    words.push(currentWord);
+}
+
+console.log(words);
