4b6a78436f7abb90be3b5e014172ce50f47c0691
Jobael Ahmed Nishat <159699848+Jobael22@users.noreply.github.com>
2024-03-07 16:12:29 +0600

    Update script0.js

--- script0.js
+++ script0.js
@@ -1,7 +1,8 @@
-
-string_to_array = function (str) {
-    
-    return str.trim().split(" ");
-};
-
 console.log(string_to_array("Robin Singh"));
+
+function string_to_array(input)
+{
+let array = input.split(" ");
+
+return array;
+}
